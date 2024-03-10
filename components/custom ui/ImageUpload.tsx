7c37f66ef63ff83e95d85cb69555aebb52b1562b
johnsethsalazar <salazarjohnseth@gmail.com>
2024-03-10 19:52:38 +0800

    Added title, description, and image components for the create new collection page

---
+++ components/custom ui/ImageUpload.tsx
@@ -0,0 +1,19 @@
+import { CldUploadWidget } from 'next-cloudinary';
+import { Button } from '../ui/button';
+import { Plus } from 'lucide-react';
+
+const ImageUpload = () => {
+  return (
+    <CldUploadWidget uploadPreset="muim0fwq">
+      {({ open }) => {
+        return (
+          <Button onClick={() => open()} className='bg-grey-1 text-white'>
+            <Plus  className='h-4 w-4 mr-2'/>Upload Image
+          </Button>
+        );
+      }}
+    </CldUploadWidget>
+  )
+}
+
+export default ImageUpload
