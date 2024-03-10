26543cb51f26495a6b110ce9269806c3c3b23910
johnsethsalazar <salazarjohnseth@gmail.com>
2024-03-10 09:10:30 +0800

    Added Collection page heading and separator

---
+++ components/collections/CollectionForm.tsx
@@ -0,0 +1,12 @@
+import { Separator } from "../ui/separator"
+
+const CollectionForm = () => {
+  return (
+    <div className='p-10'>
+      <p className='text-heading2-bold'>Create Collection</p>
+      <Separator className="bg-grey-1 my-4 mb-7"/>
+    </div>
+  )
+}
+
+export default CollectionForm
