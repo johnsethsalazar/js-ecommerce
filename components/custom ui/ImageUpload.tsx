9d34844ff53cb524dcec610314cf5a1066f7813d
johnsethsalazar <salazarjohnseth@gmail.com>
2024-03-10 21:24:25 +0800

    Added onRemove function - A button to remove the uploaded image in the new collection page

--- components/custom ui/ImageUpload.tsx
+++ components/custom ui/ImageUpload.tsx
@@ -1,6 +1,6 @@
 import { CldUploadWidget } from "next-cloudinary";
 import { Button } from "../ui/button";
-import { Plus } from "lucide-react";
+import { Plus, Trash } from "lucide-react";
 import React from "react";
 import Image from "next/image";
 import { url } from "inspector";
@@ -25,13 +25,19 @@ const ImageUpload: React.FC<ImageUploadProps> = ({
       <div className="mb-4 flex flex-wrap item-center gap-4">
         {value.map((url) => (
           // eslint-disable-next-line react/jsx-key
-          <Image
-            src={url}
-            alt="collection"
-            className="object-cover rounded-lg"
-            width={200}
-            height={200}
-          />
+          <div className="relative w-[200px] h-[200px]">
+            <div className="absolute top-0 right-0 z-10">
+              <Button onClick={() => onRemove(url)} size="sm" className="bg-red-1 text-white">
+                <Trash className="h-4 w-4"/>
+              </Button>
+            </div>
+            <Image
+              src={url}
+              alt="collection"
+              className="object-cover rounded-lg"
+              layout="fill"
+            />
+          </div>
         ))}
       </div>
       <CldUploadWidget uploadPreset="muim0fwq" onUpload={onUpload}>
