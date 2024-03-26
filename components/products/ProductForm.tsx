92f4000ab5ea6d10aab4245f52692912f354a39c
johnsethsalazar <salazarjohnseth@gmail.com>
2024-03-26 11:47:29 +0800

    Added this as a fix for when the enter is pressed the ImageUpload is being loaded for the ProductForm page.

--- components/products/ProductForm.tsx
+++ components/products/ProductForm.tsx
@@ -1,10 +1,10 @@
-"use client"
+"use client";
 
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
-import { Separator } from "../ui/separator"
-import { z } from "zod"
-import { Button } from "@/components/ui/button"
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { Separator } from "../ui/separator";
+import { z } from "zod";
+import { Button } from "@/components/ui/button";
 import {
   Form,
   FormControl,
@@ -13,14 +13,15 @@ import {
   FormItem,
   FormLabel,
   FormMessage,
-} from "@/components/ui/form"
-import { Input } from "@/components/ui/input"
-import { Textarea } from "../ui/textarea"
-import ImageUpload from "../custom ui/ImageUpload"
-import { useParams, useRouter } from "next/navigation"
-import { useState } from "react"
-import toast from "react-hot-toast"
-import Delete from "../custom ui/Delete"
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "../ui/textarea";
+import ImageUpload from "../custom ui/ImageUpload";
+import { useParams, useRouter } from "next/navigation";
+import { useState } from "react";
+import toast from "react-hot-toast";
+import Delete from "../custom ui/Delete";
+import MultiText from "../custom ui/MultiText";
 
 const formSchema = z.object({
   title: z.string().min(2).max(20),
@@ -33,13 +34,13 @@ const formSchema = z.object({
   colors: z.array(z.string()),
   price: z.coerce.number().min(0.1),
   expense: z.coerce.number().min(0.1),
-})
+});
 
 interface ProductFormProps {
-  initialData?: ProductType | null // Must have ? to make it optional
+  initialData?: ProductType | null; // Must have ? to make it optional
 }
 
-const ProductForm: React.FC<ProductFormProps> = ({initialData}) => {
+const ProductForm: React.FC<ProductFormProps> = ({ initialData }) => {
   const router = useRouter();
   //const params = useParams();
 
@@ -47,59 +48,69 @@ const ProductForm: React.FC<ProductFormProps> = ({initialData}) => {
   // 1. Define your form.
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
-    defaultValues: initialData ? initialData : {
-      title: "",
-      description: "",
-      media: [],
-      category: "",
-      collections: [],
-      tags: [],
-      sizes: [],
-      colors: [],
-      price: 0.1,
-      expense: 0.1,
-    },
-  })
+    defaultValues: initialData
+      ? initialData
+      : {
+          title: "",
+          description: "",
+          media: [],
+          category: "",
+          collections: [],
+          tags: [],
+          sizes: [],
+          colors: [],
+          price: 0.1,
+          expense: 0.1,
+        },
+  });
 
-  const handleKeyPress = (e: React.KeyboardEvent<HTMLInputElement> | React.KeyboardEvent<HTMLTextAreaElement>) => {
-    if(e.key === "Enter"){ 
-      e.preventDefault(); 
+  const handleKeyPress = (
+    e:
+      | React.KeyboardEvent<HTMLInputElement>
+      | React.KeyboardEvent<HTMLTextAreaElement>
+  ) => {
+    if (e.key === "Enter") {
+      e.preventDefault();
     }
-  }
+  };
 
-   // 2. Define a submit handler.
-   const onSubmit = async (values: z.infer<typeof formSchema>) => {
+  // 2. Define a submit handler.
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
     // Do something with the form values.
     // ✅ This will be type-safe and validated.
     //console.log(values)
-    try{
+    try {
       setLoading(true);
-      const url = initialData ? `/api/collections/${initialData._id}` : "/api/collections";
+      const url = initialData
+        ? `/api/products/${initialData._id}`
+        : "/api/products";
       const res = await fetch(url, {
         method: "POST",
         body: JSON.stringify(values),
-      })
-      if(res.ok){
+      });
+      if (res.ok) {
         setLoading(false);
-        toast.success(`Collection ${initialData ? "updated" : "created"}.`);
-        window.location.href = "/collections";
-        router.push("/collections");
+        toast.success(`Product ${initialData ? "updated" : "created"}.`);
+        window.location.href = "/products";
+        router.push("/products");
       }
-    }catch(err){
-      console.log("[collections_POST]", err);
+    } catch (err) {
+      console.log("[products_POST]", err);
       toast.error("Something went wrong. Please try again.");
     }
-  }
+  };
 
   return (
-    <div className='p-10'>
-    {initialData ? (
-    <div className="flex items-center justify-between">
-      <p className="text-heading2-bold">Edit Collection</p>
-      <Delete id={initialData._id} />
-    </div>) 
-    : (<p className="text-heading2-bold">Create Collection</p>)}
-      <Separator className="bg-grey-1 my-4 mb-7"/>
+    <div className="p-10">
+      {initialData ? (
+        <div className="flex items-center justify-between">
+          <p className="text-heading2-bold">Edit Product</p>
+          <Delete id={initialData._id} />
+        </div>
+      ) : (
+        <p className="text-heading2-bold">Create Product</p>
+      )}
+      <Separator className="bg-grey-1 my-4 mb-7" />
       <Form {...form}>
         <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
           <FormField
@@ -109,11 +120,13 @@ const ProductForm: React.FC<ProductFormProps> = ({initialData}) => {
               <FormItem>
                 <FormLabel>Title</FormLabel>
                 <FormControl>
-                  <Input placeholder="Title" {...field} onKeyDown={handleKeyPress}/>
+                  <Input
+                    placeholder="Title"
+                    {...field}
+                    onKeyDown={handleKeyPress}
+                  />
                 </FormControl>
-                <FormDescription>
-                  The title of the collection.
-                </FormDescription>
+                <FormDescription>The title of the product.</FormDescription>
                 <FormMessage />
               </FormItem>
             )}
@@ -125,10 +138,15 @@ const ProductForm: React.FC<ProductFormProps> = ({initialData}) => {
               <FormItem>
                 <FormLabel>Description</FormLabel>
                 <FormControl>
-                  <Textarea placeholder="Description" {...field} rows={5} onKeyDown={handleKeyPress}/>
+                  <Textarea
+                    placeholder="Description"
+                    {...field}
+                    rows={5}
+                    onKeyDown={handleKeyPress}
+                  />
                 </FormControl>
                 <FormDescription>
-                  The description of the collection.
+                  The description of the product.
                 </FormDescription>
                 <FormMessage />
               </FormItem>
@@ -136,31 +154,123 @@ const ProductForm: React.FC<ProductFormProps> = ({initialData}) => {
           />
           <FormField
             control={form.control}
-            name="image"
+            name="media"
             render={({ field }) => (
               <FormItem>
                 <FormLabel>Image</FormLabel>
                 <FormControl>
-                  <ImageUpload 
-                    value={field.value ? [field.value] : []} 
-                    onChange={(url) => field.onChange(url)} 
-                    onRemove={() => field.onChange("")} />
+                  <ImageUpload
+                    value={field.value}
+                    onChange={(url) => field.onChange([...field.value, url])}
+                    onRemove={(url) =>
+                      field.onChange([
+                        ...field.value.filter((image) => image !== url),
+                      ])
+                    }
+                  />
                 </FormControl>
-                <FormDescription>
-                  The image of the collection.
-                </FormDescription>
+                <FormDescription>The image of the product.</FormDescription>
                 <FormMessage />
               </FormItem>
             )}
           />
+          <div className="md: grid md:grid-cols-3 md:gap-8">
+            <FormField
+              control={form.control}
+              name="price"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Price ($)</FormLabel>
+                  <FormControl>
+                    <Input
+                      type="number"
+                      placeholder="Price"
+                      {...field}
+                      onKeyDown={handleKeyPress}
+                    />
+                  </FormControl>
+                  <FormDescription>The price of the product.</FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="expense"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Expense ($)</FormLabel>
+                  <FormControl>
+                    <Input
+                      type="number"
+                      placeholder="Expense"
+                      {...field}
+                      onKeyDown={handleKeyPress}
+                    />
+                  </FormControl>
+                  <FormDescription>The expense of the product.</FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="category"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Category</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="Category"
+                      {...field}
+                      onKeyDown={handleKeyPress}
+                    />
+                  </FormControl>
+                  <FormMessage className="text-red-1" />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="tags"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Tags</FormLabel>
+                  <FormControl>
+                    <MultiText
+                      placeholder="Tags"
+                      value={field.value}
+                      onChange={(tag) => field.onChange([...field.value, tag])}
+                      onRemove={(tagToRemove) =>
+                        field.onChange([
+                          ...field.value.filter((tag) => tag !== tagToRemove),
+                        ])
+                      }
+                    />
+                  </FormControl>
+                  <FormMessage className="text-red-1" />
+                </FormItem>
+              )}
+            />
+          </div>
           <div className="flex gap-10">
-            <Button type="submit" className="bg-blue-1 text-white">Submit</Button>
-            <Button type="button" onClick={() => router.push("/collections")} className="bg-blue-1 text-white">Discard</Button>
+            <Button type="submit" className="bg-blue-1 text-white">
+              Submit
+            </Button>
+            <Button
+              type="button"
+              onClick={() => router.push("/collections")}
+              className="bg-blue-1 text-white"
+            >
+              Discard
+            </Button>
           </div>
         </form>
       </Form>
     </div>
-  )
-}
+  );
+};
 
-export default ProductForm
+export default ProductForm;
+
+// 3:02:52 - Pressing enter opens the upload image. Fix this.
