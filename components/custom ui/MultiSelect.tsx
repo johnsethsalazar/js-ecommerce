5ab8e9e3e952652e28cb1b6c30cf7470067ac60b
johnsethsalazar <salazarjohnseth@gmail.com>
2024-03-29 15:56:02 +0800

    Added a feature that shows the selected collection title and console logging it's id in the console through the /product/new page

--- components/custom ui/MultiSelect.tsx
+++ components/custom ui/MultiSelect.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { useState } from "react";
 
 import {
@@ -11,13 +13,14 @@ import {
   CommandSeparator,
   CommandShortcut,
 } from "@/components/ui/command";
+import { Badge } from "../ui/badge";
 
 interface MultiSelectProps {
-  placeholder: string
-  collections: CollectionType[]
-  value: string[]
-  onChange: (value: string) => void
-  onRemove: (value: string) => void
+  placeholder: string;
+  collections: CollectionType[];
+  value: string[];
+  onChange: (value: string) => void;
+  onRemove: (value: string) => void;
 }
 
 const MultiSelect: React.FC<MultiSelectProps> = ({
@@ -27,23 +30,49 @@ const MultiSelect: React.FC<MultiSelectProps> = ({
   onChange,
   onRemove,
 }) => {
-  const [inputValue, setInputValue] = useState('');
+  const [inputValue, setInputValue] = useState("");
   const [open, setOpen] = useState(false);
+
+  //console.log(value)
+  let selected: CollectionType[];
+
+  if (value.length === 0) {
+    selected = [];
+  } else {
+    selected = value.map((id) =>
+      collections.find((collection) => collection._id === id)
+    ) as CollectionType[];
+  }
 
   return (
     <Command className="overflow-visible bg-white">
-      <CommandInput
-        placeholder={placeholder}
-        value={inputValue}
-        onValueChange={setInputValue}
-        onBlur={() => setOpen(false)}
-        onFocus={() => setOpen(true)}
-      />
+      <div className="flex gap-1 flex-wrap border rounded-md">
+        {selected.map((collection) => (
+          <Badge key={collection._id}>{collection.title}</Badge>
+        ))}
+        <CommandInput
+          placeholder={placeholder}
+          value={inputValue}
+          onValueChange={setInputValue}
+          onBlur={() => setOpen(false)}
+          onFocus={() => setOpen(true)}
+        />
+      </div>
       <div className="relative mt-2">
         {open && (
           <CommandGroup className="absolute w-full z-10 top-0 overflow-auto border rounded-md shadow-md">
             {collections.map((collection) => (
-              <CommandItem key={collection._id}>{collection.title}</CommandItem>
+              <CommandItem
+                key={collection._id}
+                onMouseDown={(e) => {
+                  e.preventDefault();
+                }}
+                onSelect={() => {
+                  onChange(collection._id);
+                }}
+              >
+                {collection.title}
+              </CommandItem>
             ))}
           </CommandGroup>
         )}
