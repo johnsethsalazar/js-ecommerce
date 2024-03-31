eeae495d5434d2c0475031ae4530e1e3950212ff
johnsethsalazar <salazarjohnseth@gmail.com>
2024-03-31 15:28:25 +0800

    added the create product button's feature in the /products page and the discard button feature in the /products/new page

--- app/(dashboard)/products/page.tsx
+++ app/(dashboard)/products/page.tsx
@@ -40,7 +40,7 @@ const Products = () => {
         <p className="text-heading2-bold">Products</p>
         <Button
           className="bg-blue-1 text-white"
-          onClick={() => router.push("/collections/new")}
+          onClick={() => router.push("/products/new")}
         >
           <Plus className="h-4 w-4 mr-2" />
           Create Product
