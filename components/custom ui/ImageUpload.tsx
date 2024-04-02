b9bd005b989b9e0a62dad1291751fb2bd76d50c0
johnsethsalazar <salazarjohnseth@gmail.com>
2024-04-02 10:22:53 +0800

    Added a fix in order for the Upload Image button to not submit the form when clicked. The fix is adding type=button

--- components/custom ui/ImageUpload.tsx
+++ components/custom ui/ImageUpload.tsx
@@ -43,7 +43,7 @@ const ImageUpload: React.FC<ImageUploadProps> = ({
       <CldUploadWidget uploadPreset="muim0fwq" onUpload={onUpload}>
         {({ open }) => {
           return (
-            <Button onClick={() => open()} className="bg-grey-1 text-white">
+            <Button type="button" onClick={() => open()} className="bg-grey-1 text-white">
               <Plus className="h-4 w-4 mr-2" />
               Upload Image
             </Button>
