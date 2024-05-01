20d26dc836cee2df070da720bd5910d96aff613a
johnsethsalazar <salazarjohnseth@gmail.com>
2024-05-01 14:20:15 +0800

    Added a Total Revenue Card Component to the Dashboard page

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,9 +1,32 @@
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Separator } from "@/components/ui/separator";
+import { getSalesPerMonth, getTotalCustomers, getTotalSales } from "@/lib/actions/actions";
 import { UserButton } from "@clerk/nextjs";
+import { CircleDollarSign } from "lucide-react";
 
-export default function Home() {
+export default async function Home() {
+  const totalRevenue = await getTotalSales().then((data) => data.totalRevenue);
+  const totalOrders = await getTotalSales().then((data) => data.totalOrders);
+  const totalCustomers = await getTotalCustomers
+
+  const graphData = await getSalesPerMonth();
+
   return (
-    <div>
-      <UserButton />
+    <div className="px-8 py-10">
+      <p className="text-heading2-bold">Dashboard</p>
+      <Separator className="bg-grey-1 my-5" />
+
+      <div className="grid grid-cols-2 md:grid-cols-3 gap-10">
+        <Card>
+          <CardHeader className="flex flex-row justify-between items-center">
+            <CardTitle>Total Revenue</CardTitle>
+            <CircleDollarSign className="max-sm:hidden"/>
+          </CardHeader>
+          <CardContent>
+            <p className="text-body-bold">${totalRevenue}</p>
+          </CardContent>
+        </Card>
+      </div>
     </div>
   );
 }
